23c70a46228e315d996eabfc47d7371d7d17a85c
Akshay Verma <akshayverma295@gmail.com>
2017-05-24 16:11:48 +0530

    Add App Config file

---
+++ src/components/ConfigMap.js
@@ -0,0 +1,14 @@
+let appConfig = {
+	"app.title" : "Story Generator",
+	"app.version" : "ALPHA"
+	
+};
+
+let homeComponent = {
+	"primary_header": "OpenBudgets India",
+	"secondary_header" : "Story Generator",
+	"description" : "Select an indicator to generate the visualizations"
+}
+
+
+export {appConfig, homeComponent};
