60ed1715fb387208b32ed81bdcb2e383e24e6780
Pooja Shah <pooja@moengage.com>
2017-06-04 16:46:54 +0530

    added docstring

--- src/util/indicators_parser.py
+++ src/util/indicators_parser.py
@@ -5,171 +5,11 @@ class IndicatorsParser(object):
 
     c = []
 
-    def createSHC(self, row, key):
-        return {
-            "SHC": row["SHC"],
-            "figures": [
-                {
-                    "ActualTargetAchievedPercentage": [
-                        {
-                            "June": row[key],
-                        }
-                    ]
-                }
-            ]
-        }
-
-    def createRow(self, row):
-        return [
-            {
-                "indicatorGroup": "ANC",
-                "subIndicators": [
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "percentage_pregnant_women_registered_before_12_weeks_against_total"),
-                        ],
-                        "indicator": "% registered before 12 weeks",
-                        "slugIndicator": "pc_reg_before_12_weeeks",
-                        "unit": "Percentage",
-                    },
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "percentage_pregnant_women_received_3_checkups"),
-                        ],
-                        "indicator": "% received 3 checkups",
-                        "slugIndicator": "pc_rec_3_checkups",
-                        "unit": "Percentage",
-                    },
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "percentage_pregnant_women_given_TT_and_booster"),
-                        ],
-                        "indicator": "% women given tt 1-2 & booster",
-                        "slugIndicator": "pc_rec_tt_booster",
-                        "unit": "Percentage",
-                    },
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "percentage_pregnant_women_given_IFA_tables"),
-                        ],
-                        "indicator": "% women given ifa tablets",
-                        "slugIndicator": "pc_rec_ifa_tablets",
-                        "unit": "Percentage",
-                    },
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "Total number of HRP "),
-                        ],
-                        "indicator": "# of hrp",
-                        "slugIndicator": "#_of_hrp",
-                        "unit": "Number",
-                    },
-                ]
-            },
-            {
-                "indicatorGroup": "Immunization",
-                "subIndicators": [
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "percentage_given_zero_dose_to_children_against_total_live_birth"),
-                        ],
-                        "indicator": "% zero dose childrens",
-                        "slugIndicator": "pc_zero_dose_children",
-                        "unit": "Percentage",
-                    },
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "percentatge_childeren_full_immunized"),
-                        ],
-                        "indicator": "% children full immunized",
-                        "slugIndicator": "pc_children_full_immunized",
-                        "unit": "Percentage",
-                    },
-                ]
-            },
-            {
-                "indicatorGroup": "Deliveries",
-                "subIndicators": [
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "Number of institutional deliveries against to total number of deliveries"),
-                        ],
-                        "indicator": "# institutional deliveries",
-                        "slugIndicator": "#_institutional_deliveries",
-                        "unit": "Number",
-                    },
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row,
-                                      "Number of Complicated pregnancies treated including C-Section     (      )"),
-                        ],
-                        "indicator": "# complicated pregnancies",
-                        "slugIndicator": "#_complicated_pregnancies",
-                        "unit": "Number",
-                    },
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "Number of delievery cases reffered at higher facilities"),
-                        ],
-                        "indicator": "# delivery cases reffered",
-                        "slugIndicator": "#_delivery_cases_reffered",
-                        "unit": "Number",
-                    },
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "Total number of live birth"),
-                        ],
-                        "indicator": "# of live birth",
-                        "slugIndicator": "#_live_birth",
-                        "unit": "Number",
-                    },
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "Number of Newborn having weighed > 2.5 kg"),
-                        ],
-                        "indicator": "# of newborn > 2.5kg",
-                        "slugIndicator": "#_newborn_2.5kg",
-                        "unit": "Number",
-                    },
-                ]
-            },
-            {
-                "indicatorGroup": "PostNatalCare",
-                "subIndicators": [
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "Women receiving post partum check-up within 48 hours after delivery"),
-                        ],
-                        "indicator": "women post partum",
-                        "slugIndicator": "#_women_post_partum",
-                        "unit": "Number",
-                    },
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "New borns received 6 post natal HBNC visit"),
-                        ],
-                        "indicator": "New born post natal",
-                        "slugIndicator": "#_new_born_post_natal",
-                        "unit": "Number",
-                    },
-                ]
-            },
-            {
-                "indicatorGroup": "NoOfDeaths",
-                "subIndicators": [
-                    {
-                        "SHCFigures": [
-                            self.createSHC(row, "MMR"),
-                        ],
-                        "indicator": "mmr",
-                        "slugIndicator": "#_mmr",
-                        "unit": "Number",
-                    },
-                ]
-            },
-        ]
-
-    def getIndicatorJsonFromCsv(self, csv_file_name):
+    def get_indicator_json_from_csv(self, csv_file_name):
+        """
+        takes csv & constructs required json
+        :param csv_file_name: string file name
+        """
         with open(csv_file_name) as csvfile:
             reader = csv.DictReader(csvfile)
             for index, row in enumerate(reader):
@@ -254,10 +94,188 @@ class IndicatorsParser(object):
                                     self.createSHC(row, "MMR"),
                                 )
 
+
         data = json.dumps(c)
         with open("indicators.json", 'w') as json_file:
             json_file.write(data)
         print "json extracted in indicators.json"
+
+    def createSHC(self, row, key):
+        """
+        create SHC level json
+        :param row:
+        :param key:
+        :return: SHC level json
+        """
+        return {
+            "SHC": row["SHC"],
+            "figures": [
+                {
+                    "ActualTargetAchievedPercentage": [
+                        {
+                            "June": row[key],
+                        }
+                    ]
+                }
+            ]
+        }
+
+    def createRow(self, row):
+        """
+            create SHC level json
+            :param row:
+            :param key:
+            :return: SHC level json
+            """
+        return [
+            {
+                "indicatorGroup": "ANC",
+                "subIndicators": [
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "percentage_pregnant_women_registered_before_12_weeks_against_total"),
+                        ],
+                        "indicator": "% registered before 12 weeks",
+                        "slugIndicator": "pc_reg_before_12_weeeks",
+                        "unit": "Percentage",
+                    },
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "percentage_pregnant_women_received_3_checkups"),
+                        ],
+                        "indicator": "% received 3 checkups",
+                        "slugIndicator": "pc_rec_3_checkups",
+                        "unit": "Percentage",
+                    },
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "percentage_pregnant_women_given_TT_and_booster"),
+                        ],
+                        "indicator": "% women given tt 1-2 & booster",
+                        "slugIndicator": "pc_rec_tt_booster",
+                        "unit": "Percentage",
+                    },
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "percentage_pregnant_women_given_IFA_tables"),
+                        ],
+                        "indicator": "% women given ifa tablets",
+                        "slugIndicator": "pc_rec_ifa_tablets",
+                        "unit": "Percentage",
+                    },
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "Total number of HRP "),
+                        ],
+                        "indicator": "# of hrp",
+                        "slugIndicator": "#_of_hrp",
+                        "unit": "Number",
+                    },
+                ]
+            },
+            {
+                "indicatorGroup": "Immunization",
+                "subIndicators": [
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "percentage_given_zero_dose_to_children_against_total_live_birth"),
+                        ],
+                        "indicator": "% zero dose childrens",
+                        "slugIndicator": "pc_zero_dose_children",
+                        "unit": "Percentage",
+                    },
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "percentatge_childeren_full_immunized"),
+                        ],
+                        "indicator": "% children full immunized",
+                        "slugIndicator": "pc_children_full_immunized",
+                        "unit": "Percentage",
+                    },
+                ]
+            },
+            {
+                "indicatorGroup": "Deliveries",
+                "subIndicators": [
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row,
+                                           "Number of institutional deliveries against to total number of deliveries"),
+                        ],
+                        "indicator": "# institutional deliveries",
+                        "slugIndicator": "#_institutional_deliveries",
+                        "unit": "Number",
+                    },
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row,
+                                           "Number of Complicated pregnancies treated including C-Section     (      )"),
+                        ],
+                        "indicator": "# complicated pregnancies",
+                        "slugIndicator": "#_complicated_pregnancies",
+                        "unit": "Number",
+                    },
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "Number of delievery cases reffered at higher facilities"),
+                        ],
+                        "indicator": "# delivery cases reffered",
+                        "slugIndicator": "#_delivery_cases_reffered",
+                        "unit": "Number",
+                    },
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "Total number of live birth"),
+                        ],
+                        "indicator": "# of live birth",
+                        "slugIndicator": "#_live_birth",
+                        "unit": "Number",
+                    },
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "Number of Newborn having weighed > 2.5 kg"),
+                        ],
+                        "indicator": "# of newborn > 2.5kg",
+                        "slugIndicator": "#_newborn_2.5kg",
+                        "unit": "Number",
+                    },
+                ]
+            },
+            {
+                "indicatorGroup": "PostNatalCare",
+                "subIndicators": [
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "Women receiving post partum check-up within 48 hours after delivery"),
+                        ],
+                        "indicator": "women post partum",
+                        "slugIndicator": "#_women_post_partum",
+                        "unit": "Number",
+                    },
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "New borns received 6 post natal HBNC visit"),
+                        ],
+                        "indicator": "New born post natal",
+                        "slugIndicator": "#_new_born_post_natal",
+                        "unit": "Number",
+                    },
+                ]
+            },
+            {
+                "indicatorGroup": "NoOfDeaths",
+                "subIndicators": [
+                    {
+                        "SHCFigures": [
+                            self.createSHC(row, "MMR"),
+                        ],
+                        "indicator": "mmr",
+                        "slugIndicator": "#_mmr",
+                        "unit": "Number",
+                    },
+                ]
+            },
+        ]
 
 
 
@@ -267,4 +285,4 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print "Please give CSV file path (Export CSV from the Indicator's Sheet1"
         sys.exit(0)
-    indicator.getIndicatorJsonFromCsv(sys.argv[1])
+    indicator.get_indicator_json_from_csv(sys.argv[1])
